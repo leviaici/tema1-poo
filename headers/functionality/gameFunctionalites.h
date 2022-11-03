95d053cd3ce55fc670e1ce2f7668f99567927412
Levi <adrian-george.leventiu@s.unibuc.ro>
2022-11-03 15:22:31 +0200

    Added deleteAccount functions

---
+++ headers/functionality/gameFunctionalites.h
@@ -0,0 +1,8 @@
+//
+// Created by Adrian Leventiu on 03.11.2022.
+//
+
+#ifndef OOP_GAMEFUNCTIONALITES_H
+#define OOP_GAMEFUNCTIONALITES_H
+
+#endif //OOP_GAMEFUNCTIONALITES_H
--- main.cpp
+++ main.cpp
@@ -1,27 +1,12 @@
 #include <vector>
 #include <fstream>
-#include <ctime>
 #include <thread>
 #include <chrono>
-#include "headers/University.h"
+
+#include "headers/functionality/functionalities.h"
 
 void loginAccount(int counter);
 void registerAccount();
-
-std::string toLower(std::string word) {
-    if(word[0] >= 65 && word[0] <= 90)
-        word[0] += 32;
-    for(unsigned i = 1; i < word.size(); i++)
-        if(word[i] == '-' && word[i + 1] >= 65 && word[i + 1] <= 90)
-            word[i + 1] += 32;
-    return word;
-}
-std::string toLowerWholeWord(std::string word) {
-    for(char &letter : word)
-        if(letter >= 65 && letter <= 90)
-            letter += 32;
-    return word;
-}
 
 std::string checkAccountType(std::string accountType) {
     if(accountType == "student" || accountType == "professor")
@@ -31,57 +16,6 @@ std::string checkAccountType(std::string accountType) {
     if(accountType[0] == 'p' && accountType[1] == 'r')
         return "professor";
     return "wrong";
-}
-
-void showLocalTime() {
-    time_t now = time(nullptr);
-
-    tm *ltm = localtime(&now);
-
-    std::cout << "Your current date: " << ltm -> tm_mday << "/" << 1 + ltm -> tm_mon << "/" << 1900 + ltm -> tm_year << "\n";
-    std::cout << "Your current time: " << ltm -> tm_hour << ":" << ltm -> tm_min << ":" << ltm -> tm_sec << "\n";
-}
-
-void test() {
-    std::vector<Professor> p;
-
-    Professor p1 = Professor("Marius", "Micluta", "Informatica", "marius.micluta@unibuc.ro", "Unknown", 23, 02, 1999);
-    Professor p2 = Professor("Radu", "Boriga", "Informatica", "radu.boriga@unibuc.ro", "Unknown", 20, 7 ,1975);
-    p.push_back(p1); p.push_back(p2);
-
-    std::vector<Student> s;
-
-    Student s1 = Student("Adrian", "Leventiu", "adrian.leventiu@s.unibuc.ro", "Unknown", 262, 31, 1, 2003);
-    Student s2 = Student("Stefan", "Radu", "stefan.radu@s.unibuc.ro", "Unknown", 263, 19, 5, 2002);
-    s.push_back(s1); s.push_back(s2);
-
-    University unibuc("Universitatea din Bucuresti", "Matematica si Informatica", 1864, {}, {});
-    unibuc.add_professor(Professor("Liliana", "Mitre", "Matematica", "liliana.mitre@unibuc.ro", "Unknown", 20, 1, 1990));
-    unibuc.add_multipleProfessors(p);
-
-    unibuc.sortProfessors();
-    unibuc.print_professors();
-
-    std::vector<int> g;
-    g.push_back(4); g.push_back(8); g.push_back(7); g.push_back(5);
-
-    Subject note("Informatica", g);
-    Subject note2("Matematica", std::vector<int>{10, 10, 10, 8});
-
-    std::vector<Subject> note3, note4;
-    note3.push_back(note); note3.push_back(note2);
-    note4.push_back(note2); note4.push_back(Subject("Informatica", std::vector<int>{4, 9, 7, 5}));
-
-
-    unibuc.add_student(Student("Flavius", "Popescu", "flavius.popescu@s.unibuc.ro", "0711223344", 261, 19, 9, 2001, note3));
-    unibuc.add_multipleStudents(s);
-    unibuc.add_student(Student("Flavius", "Ratat", "flavius.popescu@s.damada.ro", "nu am", 262, 26, 6, 1996, note4));
-    unibuc.add_student(Student("Radu", "Macelaru", "radu.macelaru@s.unibuc.ro", "0711223344", 104, 19, 4, 1998, note3));
-    unibuc.print_students();
-    unibuc.reclassify();
-    unibuc.print_students();
-
-    unibuc.print_groups();
 }
 
 void blockUser(int counter) {
@@ -260,6 +194,47 @@ void registerAccount() {
     }
 }
 
+void deleteAccount(const std::string& email, const std::string& password) {
+    char choice;
+    std::cout << "Are you sure you want to permanently delete your account?\n(Y/N): ";
+    std::cin >> choice;
+
+    while(choice != 'Y' && choice != 'y' && choice != 'N' && choice != 'n') {
+        std::cout << "Asked for Y or N. Try again, please.\n";
+        std::cin >> choice;
+    }
+
+    if(choice == 'y' || choice == 'Y') {
+        std::string passwordVerification;
+        std::cout << "Enter the password for confirmation and your account will be deleted as you wish.\n";
+        std::cin >> passwordVerification;
+
+        int counter = 0;
+        while(passwordVerification != password) {
+            std::cout << "Wrong password.\n";
+            counter++;
+            if(!(counter % 3))
+                blockUser(counter);
+            std::cout << "Try again typing your password to confirm the account deletion.\n";
+            std::cin >> passwordVerification;
+        }
+
+        std::string accountType;
+        if(checkExistingAccount(email, "student"))
+            accountType = "student";
+        else accountType = "professor";
+
+        std::cout << "Deleting account...\n";
+        try {
+            if(!(std::filesystem::remove("../savings/" + accountType + "s/" + email + ".txt")))
+                throw std::exception();
+            std::cout << "Account deletion complete. All your data have been erased.\n";
+        } catch (std::exception& e) {
+            std::cout << "Error 404: Account deletion incomplete as file was not found.\n";
+        }
+    }
+}
+
 void loginAccount(int counter) {
     std::string email, password;
     std::cout << "E-mail: ";    std::cin >> email;
@@ -289,11 +264,11 @@ void loginAccount(int counter) {
 
     if(password == readPassword) {
         bool choice;
-        std::cout << "Do you want to continue the saved game (1) or do you want to delete the savings and start again (0)?\n(1/0): ";
+        std::cout << "Do you want to continue the saved game (1) or do you want to delete the savings/account and start again (0)?\n(1/0): ";
         std::cin >> choice;
         if(choice)
             std::cout << "Continuing...\n"; ///TBA CONTINUING GAME FUNCTION
-        else std:: cout << "Deleting savings...\n"; ///TBA DELETING SAVINGS FUNCTION
+        else deleteAccount(email, password);
     } else {
         std::cout << "Incorrect E-mail or password. ";
         if(counter % 3) {
