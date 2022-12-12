042e1e90d5b0995ca6151ccbfcf80ffe1416eb8d
Levi <adrian-george.leventiu@s.unibuc.ro>
2022-12-12 17:16:56 +0200

    Updated Exceptions

--- headers/Exceptions.h
+++ headers/Exceptions.h
@@ -6,25 +6,35 @@
 #define OOP_EXCEPTIONS_H
 
 #include <stdexcept>
+#include <string>
 
-class error31st : public std::runtime_error {
+class errorInput : public std::runtime_error {
     using std::runtime_error::runtime_error;
 };
 
-class errorYoung : public std::runtime_error {
-    using std::runtime_error::runtime_error;
+class error31st : public errorInput {
+public:
+    explicit error31st(const std::string &error) : errorInput("Error - 31st: " + error) {}
 };
 
-class errorDead : public std::runtime_error {
-    using std::runtime_error::runtime_error;
+class errorYoung : public errorInput {
+public:
+    explicit errorYoung(const std::string &error) : errorInput("Error - Too Young: " + error) {}
 };
 
-class errorNotBorn : public std::runtime_error {
-    using std::runtime_error::runtime_error;
+class errorDead : public errorInput {
+public:
+    explicit errorDead(const std::string &error) : errorInput("Error - Dead: " + error) {}
 };
 
-class error29 : public std::runtime_error {
-    using std::runtime_error::runtime_error;
+class errorNotBorn : public errorInput {
+public:
+    explicit errorNotBorn(const std::string &error) : errorInput("Error - Not Born Yet: " + error) {}
+};
+
+class error29 : public errorInput {
+public:
+    explicit error29(const std::string &error) : errorInput("Error - 29: " + error) {}
 };
 
 #endif //OOP_EXCEPTIONS_H
